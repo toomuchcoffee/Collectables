364aa29d097dd30a28bc7f52abe37c82a43639b7
toomuchcoffee <geraldsander@gmx.de>
2016-06-01 15:35:59 +0200

    remove model parameter from REST calls

--- src/main/java/de/toomuchcoffee/controller/CollectibleController.java
+++ src/main/java/de/toomuchcoffee/controller/CollectibleController.java
@@ -3,7 +3,6 @@ package de.toomuchcoffee.controller;
 import de.toomuchcoffee.model.services.CollectibleService;
 import de.toomuchcoffee.view.CollectibleDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,7 +16,7 @@ public class CollectibleController {
     private CollectibleService collectibleService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public List<CollectibleDto> allCollectibles(Model model) {
+    public List<CollectibleDto> allCollectibles() {
         return collectibleService.find();
     }
 
@@ -27,12 +26,12 @@ public class CollectibleController {
     }
 
     @RequestMapping(value = "tag/{tagName}", method = RequestMethod.GET)
-    public List<CollectibleDto> collectiblesByTag(@PathVariable String tagName, Model model) {
+    public List<CollectibleDto> collectiblesByTag(@PathVariable String tagName) {
         return collectibleService.findByTagName(tagName);
     }
 
     @RequestMapping(value = "line/{productLineName}", method = RequestMethod.GET)
-    public List<CollectibleDto> collectiblesByLine(@PathVariable String productLineName, Model model) {
+    public List<CollectibleDto> collectiblesByLine(@PathVariable String productLineName) {
         return collectibleService.findByProductLineName(productLineName);
     }
 }
